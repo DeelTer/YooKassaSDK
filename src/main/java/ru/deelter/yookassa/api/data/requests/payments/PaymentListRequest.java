0d0294db5fa4df39aace4f63ed40081df124104f
Roman <roma200315@mail.ru>
2023-05-03 17:15:28 +0300

    Offset time in filters

--- src/main/java/ru/deelter/yookassa/api/data/requests/payments/PaymentListRequest.java
+++ src/main/java/ru/deelter/yookassa/api/data/requests/payments/PaymentListRequest.java
@@ -4,30 +4,28 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import ru.deelter.yookassa.api.data.PaymentStatus;
 import ru.deelter.yookassa.api.data.requests.YooRequest;
-import ru.deelter.yookassa.utils.DateFormatUtil;
 import ru.deelter.yookassa.utils.YooUrls;
 
-import java.util.Date;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class PaymentListRequest extends YooRequest {
 
-	private int limit;
-	private PaymentStatus status;
-	private final Date afterDate;
-	private final Date beforeDate;
+	private final int limit;
+	private final PaymentStatus status;
+	private final OffsetDateTime afterTime;
+	private final OffsetDateTime beforeTime;
 
-	public PaymentListRequest(int limit, PaymentStatus status, Date afterDate, Date beforeDate) {
+	public PaymentListRequest(int limit, PaymentStatus status, OffsetDateTime afterTime, OffsetDateTime beforeTime) {
 		super(YooUrls.PAYMENT_GET_LIST, RequestMethod.GET);
 		this.limit = limit;
 		this.status = status;
-		this.afterDate = afterDate;
-		this.beforeDate = beforeDate;
+		this.afterTime = afterTime;
+		this.beforeTime = beforeTime;
 	}
 
 	private PaymentListRequest(@NotNull Builder builder) {
-		this(builder.limit, builder.status, builder.afterDate, builder.beforeDate);
-		limit = builder.limit;
-		status = builder.status;
+		this(builder.limit, builder.status, builder.afterTime, builder.beforeTime);
 	}
 
 	@Contract(value = " -> new", pure = true)
@@ -40,18 +38,18 @@ public class PaymentListRequest extends YooRequest {
 		String params = "?limit=" + limit;
 		if (status != null)
 			params += "&status=" + status;
-		if (beforeDate != null)
-			params += "&created_at.lte=" + DateFormatUtil.formatISO(beforeDate);
-		if (afterDate != null)
-			params += "&created_at.gte=" + DateFormatUtil.formatISO(afterDate);
+		if (beforeTime != null)
+			params += "&created_at.lte=" + beforeTime.format(DateTimeFormatter.ISO_INSTANT);
+		if (afterTime != null)
+			params += "&created_at.gte=" + afterTime.format(DateTimeFormatter.ISO_INSTANT);
 		return params;
 	}
 
 	public static final class Builder {
 		private int limit;
 		private PaymentStatus status;
-		private Date afterDate;
-		private Date beforeDate;
+		private OffsetDateTime afterTime;
+		private OffsetDateTime beforeTime;
 
 		private Builder() {
 		}
@@ -66,13 +64,13 @@ public class PaymentListRequest extends YooRequest {
 			return this;
 		}
 
-		public Builder afterDate(Date afterDate) {
-			this.afterDate = afterDate;
+		public Builder after(OffsetDateTime afterDate) {
+			this.afterTime = afterDate;
 			return this;
 		}
 
-		public Builder beforeDate(Date beforeDate) {
-			this.beforeDate = beforeDate;
+		public Builder before(OffsetDateTime beforeDate) {
+			this.beforeTime = beforeDate;
 			return this;
 		}
 
